6cabc9ed7c6f8b6ae9ee9b31d55a6734d2ef7445
Phương Nguyễn <phuongna1908@gmail.com>
2019-06-22 21:46:52 +0700

    remove unnecessary lodash

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import _ from 'lodash'
-
 function component() {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const element = document.createElement('canvas')
+    element.id = 'glCanvas'
+    element.width = 640
+    element.height = 480
     return element;
 }
 
